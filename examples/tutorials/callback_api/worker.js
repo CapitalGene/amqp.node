d9a2a501261177bd9ea9f449c7a35f1f555f16d2
Chen Liang <code@chen.technology>
2014-10-11 22:36:18 -0700

    [examples] change no_ack to noAck
    https://github.com/squaremo/amqp.node/commit/0bc037ad7a3fd605db0d3f23bb443843e61ff023

--- examples/tutorials/callback_api/worker.js
+++ examples/tutorials/callback_api/worker.js
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
-
+'use strict';
 var amqp = require('amqplib/callback_api');
 
 function bail(err, conn) {
   console.error(err);
-  if (conn) conn.close(function() { process.exit(1); });
+  if (conn) conn.close(function () {
+    process.exit(1);
+  });
 }
 
 function on_connect(err, conn) {
   if (err !== null) return bail(err);
-  process.once('SIGINT', function() { conn.close(); });
-  
+  process.once('SIGINT', function () {
+    conn.close();
+  });
+
   var q = 'task_queue';
 
-  conn.createChannel(function(err, ch) {
+  conn.createChannel(function (err, ch) {
     if (err !== null) return bail(err, conn);
-    ch.assertQueue(q, {durable: true}, function(err, _ok) {
-      ch.consume(q, doWork, {no_ack: false});
+    ch.assertQueue(q, {
+      durable: true
+    }, function (err, _ok) {
+      ch.consume(q, doWork, {
+        noAck: false
+      });
       console.log(" [*] Waiting for messages. To exit press CTRL+C");
     });
 
@@ -24,7 +32,7 @@ function on_connect(err, conn) {
       var body = msg.content.toString();
       console.log(" [x] Received '%s'", body);
       var secs = body.split('.').length - 1;
-      setTimeout(function() {
+      setTimeout(function () {
         console.log(" [x] Done");
         ch.ack(msg);
       }, secs * 1000);
